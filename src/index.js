60cab8e68459bce3e336420f9cfaf817ca1a1790
Δ9 <eyed.by@gmail.com>
2026-06-25 09:26:00 -0500

    build(ui): wire React entry point to public popup mount [AIOUT-CONSENT-001]

---
+++ src/index.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TugOfWarDashboard from './TugOfWarDashboard';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <TugOfWarDashboard />
+    </React.StrictMode>
+  );
+}
